1c7d3400162846d85f6c1eaef63206fc6d872f70
Ellen Saylor <ellensaylor8@gmail.com>
2019-08-30 15:28:25 -0500

    set up components

---
+++ src/components/App/FeelingToday/FeelingToday.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FeelingToday extends Component {
+    render() {
+        return (
+            <div>How are you feeling today?</div>
+        )
+    }
+}
+
+export default FeelingToday;
