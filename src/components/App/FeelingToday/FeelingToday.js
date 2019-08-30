6c03f4f08468bbd5490f2a09d7e781a63fa64762
Ellen Saylor <ellensaylor8@gmail.com>
2019-08-30 15:48:23 -0500

    set up basic markup in FeelingToday component and imported provider, createStore, combineReducers into index.js to wrap App in provider and add store as attribute

--- src/components/App/FeelingToday/FeelingToday.js
+++ src/components/App/FeelingToday/FeelingToday.js
@@ -5,6 +5,8 @@ class FeelingToday extends Component {
         return (
             <div>
                 <h1>How are you feeling today?</h1>
+                <input type="number" placeholder="Scale of 1-5" />
+                <button>NEXT</button>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    combineReducers({
+
+    })
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
